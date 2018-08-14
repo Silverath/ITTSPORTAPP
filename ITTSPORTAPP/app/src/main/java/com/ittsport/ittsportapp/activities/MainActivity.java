6240801476a5e98eb6e735717cdafc60d410907b
Silverath <pabvazzam@gmail.com>
2018-08-14 20:24:03 +0200

    zubo

--- ITTSPORTAPP/app/src/main/java/com/ittsport/ittsportapp/activities/MainActivity.java
+++ ITTSPORTAPP/app/src/main/java/com/ittsport/ittsportapp/activities/MainActivity.java
@@ -1,7 +1,9 @@
-package com.ittsport.ittsportapp;
+package com.ittsport.ittsportapp.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.ittsport.ittsportapp.R;
 
 public class MainActivity extends AppCompatActivity {
 
