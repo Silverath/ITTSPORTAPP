e654b0da67715ff1f04dad978ccb5b6a7f5b4d6a
Reloxed <antoniomma97@gmail.com>
2018-08-16 15:15:04 +0200

    xd

---
+++ ITTSPORTAPP/app/src/main/java/com/ittsport/ittsportapp/activities/GrupoActivityMain.java
@@ -0,0 +1,15 @@
+package com.ittsport.ittsportapp.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.ittsport.ittsportapp.R;
+
+public class GrupoActivityMain extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_grupo_main);
+    }
+}
