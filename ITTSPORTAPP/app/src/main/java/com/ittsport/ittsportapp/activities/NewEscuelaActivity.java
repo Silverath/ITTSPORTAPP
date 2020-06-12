b71ca501549221563785733ab491cf5096e7bb4e
Reloxed <antoniomma97@gmail.com>
2020-06-12 12:33:14 +0200

    Perfil del director añadido a la creación de escuela

--- ITTSPORTAPP/app/src/main/java/com/ittsport/ittsportapp/activities/NewEscuelaActivity.java
+++ ITTSPORTAPP/app/src/main/java/com/ittsport/ittsportapp/activities/NewEscuelaActivity.java
@@ -16,13 +16,18 @@ import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
 import com.ittsport.ittsportapp.R;
 import com.ittsport.ittsportapp.models.Escuela;
 import com.ittsport.ittsportapp.models.Estado;
+import com.ittsport.ittsportapp.models.Rol;
 import com.ittsport.ittsportapp.utils.AcceptDialog;
 import com.ittsport.ittsportapp.utils.ErrorDialog;
 import com.ittsport.ittsportapp.utils.LoadingDialog;
@@ -42,9 +47,17 @@ public class NewEscuelaActivity extends AppCompatActivity {
     FirebaseFirestore db;
     Button solicitarEscuela;
     private static final int PICK_IMAGE_REQUEST = 1;
+    private int imagenPerfil = 0;
+    int logoPerfil = 0;
     private Button btnLogoEscuela;
     private ImageView ivLogoEscuela;
     private Uri uriLogoEscuela;
+    EditText nombrePerfil;
+    EditText primerApellido;
+    EditText segundoApellido;
+    Button btnPerfilDirector;
+    ImageView ivPerfilDirector;
+    Uri uriPerfilDirector;
     private StorageReference storageReference;
     LoadingDialog loadingDialog = new LoadingDialog(this);
     AcceptDialog acceptDialog = new AcceptDialog(this);
@@ -63,12 +76,20 @@ public class NewEscuelaActivity extends AppCompatActivity {
         solicitarEscuela = (Button) findViewById(R.id.btn_solicitar_escuela);
         btnLogoEscuela = (Button) findViewById(R.id.btn_escuela_logo);
         ivLogoEscuela = (ImageView) findViewById(R.id.iv_escuela_logo);
+        nombrePerfil = (EditText) findViewById(R.id.et_perfil_social_nombre);
+        primerApellido = (EditText) findViewById(R.id.et_perfil_social_primer_apellido);
+        segundoApellido = (EditText) findViewById(R.id.et_perfil_social_segundo_apellido);
+        btnPerfilDirector = (Button) findViewById(R.id.btn_perfil_imagen);
+        ivPerfilDirector = (ImageView) findViewById(R.id.iv_perfil_imagen);
+
 
         solicitarEscuela.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 if (nombre.getText().toString().equals("") || direccion.getText().toString().equals("") ||
-                        municipio.getText().toString().equals("") || provincia.getText().toString().equals("") || uriLogoEscuela == null) {
+                        municipio.getText().toString().equals("") || provincia.getText().toString().equals("") || uriLogoEscuela == null
+                || uriPerfilDirector == null || nombrePerfil.toString().equals("") || primerApellido.toString().equals("")
+                 || segundoApellido.toString().equals("")) {
                     Toast.makeText(getBaseContext(), "Rellene todos los campos por favor", Toast.LENGTH_SHORT).show();
                 } else {
                     loadingDialog.startLoadingDialog();
@@ -104,12 +125,52 @@ public class NewEscuelaActivity extends AppCompatActivity {
                                                     .addOnSuccessListener(new OnSuccessListener<Void>() {
                                                         @Override
                                                         public void onSuccess(Void aVoid) {
-                                                            loadingDialog.dismissDialog();
-                                                            acceptDialog.startAcceptDialog("Escuela creada, le notificaremos con la respuesta");
-//                                                            Intent returnIntent = new Intent();
-//                                                            returnIntent.putExtra("nuevaEscuela", nueva);
-//                                                            setResult(Activity.RESULT_OK, returnIntent);
-//                                                            finish();
+                                                            String nombreCompleto = nombrePerfil.getText().toString() + primerApellido.getText().toString() + segundoApellido.getText().toString();
+                                                            Map<String, Object> perfilDirector = new HashMap<>();
+                                                            perfilDirector.put("nombre", nombrePerfil.getText().toString());
+                                                            perfilDirector.put("primerApellido", primerApellido.getText().toString());
+                                                            perfilDirector.put("segundoApellido", segundoApellido.getText().toString());
+                                                            perfilDirector.put("cuentaUsuarioId", FirebaseAuth.getInstance().getCurrentUser().getUid());
+                                                            perfilDirector.put("nombreImagen", nombreCompleto);
+                                                            perfilDirector.put("estado", Estado.PENDIENTE.toString());
+                                                            perfilDirector.put("rol", Rol.DIRECTOR.toString());
+
+
+                                                            StorageReference perfilReference = storageReference.child(nombreCompleto + "." + getFileExtension(uriPerfilDirector));
+                                                            perfilReference.putFile(uriPerfilDirector)
+                                                                    .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                                                                        @Override
+                                                                        public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                                                                            perfilReference.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                                                                                @Override
+                                                                                public void onSuccess(Uri uri2) {
+                                                                                    perfilDirector.put("urlImagen", uri2.toString());
+                                                                                    Boolean wtf = uri.equals(uri2);
+                                                                                    db.collection("escuelas").whereEqualTo("nombre",nueva.getNombre())
+                                                                                            .whereEqualTo("provincia", nueva.getProvincia())
+                                                                                            .whereEqualTo("municipio", nueva.getMunicipio())
+                                                                                            .get().addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
+                                                                                        @Override
+                                                                                        public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
+                                                                                            String escuelaId = "";
+                                                                                            for(QueryDocumentSnapshot q: queryDocumentSnapshots){
+                                                                                                escuelaId = q.getId();
+                                                                                            }
+                                                                                            perfilDirector.put("escuelaId", escuelaId);
+                                                                                            db.collection("perfilesSociales").document()
+                                                                                                    .set(perfilDirector).addOnSuccessListener(new OnSuccessListener<Void>() {
+                                                                                                @Override
+                                                                                                public void onSuccess(Void aVoid) {
+                                                                                                    loadingDialog.dismissDialog();
+                                                                                                    acceptDialog.startAcceptDialog("Escuela creada, le notificaremos con la respuesta");
+                                                                                                }
+                                                                                            });
+                                                                                        }
+                                                                                    });
+                                                                                }
+                                                                            });
+                                                                        }
+                                                                    });
                                                         }
                                                     }).addOnFailureListener(new OnFailureListener() {
                                                 @Override
@@ -136,6 +197,15 @@ public class NewEscuelaActivity extends AppCompatActivity {
         btnLogoEscuela.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                imagenPerfil = 0;
+                openFileChooser();
+            }
+        });
+
+        btnPerfilDirector.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                imagenPerfil = 1;
                 openFileChooser();
             }
         });
@@ -152,9 +222,13 @@ public class NewEscuelaActivity extends AppCompatActivity {
         super.onActivityResult(requestCode, resultCode, data);
 
         if (requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK
-                && data != null && data.getData() != null) {
+                && data != null && data.getData() != null && imagenPerfil == 0) {
             uriLogoEscuela = data.getData();
             Picasso.with(this).load(uriLogoEscuela).into(ivLogoEscuela);
+        } else if (requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK
+                && data != null && data.getData() != null && imagenPerfil == 1) {
+            uriPerfilDirector = data.getData();
+            Picasso.with(this).load(uriPerfilDirector).into(ivPerfilDirector);
         }
     }
 
